bc438133ecd60e2686fc4f3fd06cf50b4ed573ea
Swag-blips <coderblip@gmail.com>
2024-11-03 15:02:03 +0100

    feat:swithc-up

--- config/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  port: 3000,
-  dbUri:
-    "mongodb+srv://coderblip:r0C1OPa1BoOXZLXp@cluster0.paahk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  saltWorFactor: 10,
-};
