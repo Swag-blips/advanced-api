87f7c536f7c323c5e30f8486e2a3a17c16286bae
Swag-blips <coderblip@gmail.com>
2024-11-03 18:11:14 +0100

    feat:update

---
+++ express-api/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types.js').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+};
